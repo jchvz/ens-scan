a09ecfd7bbbec974720f97afe769756e0db70200
john <j2chavez@scu.edu>
2021-12-30 15:50:39 -0800

    avail mode

--- ens.nim
+++ ens.nim
@@ -1,4 +1,4 @@
-import std/[httpclient, json, strutils, times]
+import std/[httpclient, json, strutils, os, logging]
 const url = "http://api.thegraph.com/subgraphs/name/ensdomains/ens"
 
 func gql_query(ens: string): string =
@@ -6,7 +6,7 @@ func gql_query(ens: string): string =
 
 func owner(elems: seq[JsonNode]): (string, bool) =
     if len(elems) == 0:
-        return ("no owner found", true)
+        return ("looks available!", true)
     else:
         return (elems[0]["id"].getStr, false)
 
@@ -20,10 +20,20 @@ proc get_owner(ens: string): (string, bool) =
     
     return owner(elems)
 
-let csv = open(now().format("yyyy-MM-dd hh:mm:ss tt") & ".csv", fmWrite)
+var logger: ConsoleLogger
+if "--avail" in commandLineParams():
+    logger = newConsoleLogger(lvlNotice)
+    logger.log(lvlWarn, "Printing ONLY available ENS names")
+else:
+    logger = newConsoleLogger(lvlInfo)
+    logger.log(lvlWarn, "Printing ALL ENS names")
 
 for line in lines "/in/input.csv":
-    let (owner, avail) = get_owner line
-    echo "[" & line & "] " & owner
+    let
+        (owner, avail) = get_owner line
+        msg = "[" & line & "] " & owner
+    
     if avail:
-        csv.writeLine(line)
+        logger.log(lvlNotice, msg)
+    else:
+        logger.log(lvlInfo, msg)
