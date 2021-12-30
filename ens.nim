672f1cdbc7643fcb0c163f3e609350025a13be55
john <j2chavez@scu.edu>
2021-12-30 13:19:53 -0800

    split funcs out of proc

--- ens.nim
+++ ens.nim
@@ -1,21 +1,24 @@
 import std/[httpclient, json, strutils]
 const url = "http://api.thegraph.com/subgraphs/name/ensdomains/ens"
 
-proc get_owner(ens: string): string =
-    let client = newHttpClient()
-    client.headers = newHttpHeaders({ "Content-Type": "application/json" })
-    let body = %*{
-        "query": "query{domains(where:{name:\"" & toLower(ens) & ".eth\"}){id}}"
-    }
-    let
-        response = client.request(url, httpMethod = HttpPost, body = $body)
-        parsed = response.body.parseJson
-        elems = parsed["data"]["domains"].getElems
+func gql_query(ens: string): string =
+    return "query{domains(where:{name:\"" & toLower(ens) & ".eth\"}){id}}"
 
+func owner(elems: seq[JsonNode]): string =
     if len(elems) == 0:
         return "no owner found"
     else:
         return elems[0]["id"].getStr
+
+proc get_owner(ens: string): string =
+    let
+        body = %*{"query": gql_query(ens)}
+        client = newHttpClient()
+        response = client.request(url, httpMethod = HttpPost, body = $body)
+        parsed = response.body.parseJson
+        elems = parsed["data"]["domains"].getElems
+    
+    return owner(elems)
 
 echo "name,owner"
 for line in lines "/in/input.csv":
