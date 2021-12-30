6a19333d858fb9d31bc73bcd13ac8759dad0dbde
john <j2chavez@scu.edu>
2021-12-30 14:58:03 -0800

    fix minute + multiple return

--- ens.nim
+++ ens.nim
@@ -4,13 +4,13 @@ const url = "http://api.thegraph.com/subgraphs/name/ensdomains/ens"
 func gql_query(ens: string): string =
     return "query{domains(where:{name:\"" & toLower(ens) & ".eth\"}){id}}"
 
-func owner(elems: seq[JsonNode]): string =
+func owner(elems: seq[JsonNode]): (string, bool) =
     if len(elems) == 0:
-        return "no owner found"
+        return ("no owner found", true)
     else:
-        return elems[0]["id"].getStr
+        return (elems[0]["id"].getStr, false)
 
-proc get_owner(ens: string): string =
+proc get_owner(ens: string): (string, bool) =
     let
         body = %*{"query": gql_query(ens)}
         client = newHttpClient()
@@ -20,10 +20,9 @@ proc get_owner(ens: string): string =
     
     return owner(elems)
 
-var csv = open(now().format("yyyy-MM-dd hh:MM:ss tt") & ".csv", fmWrite)
+var csv = open(now().format("yyyy-MM-dd hh:mm:ss tt") & ".csv", fmWrite)
 
 for line in lines "/in/input.csv":
-    let owner = get_owner line
+    let (owner, avail) = get_owner line
     echo "[" & line & "] " & owner
-    if owner == "no owner found":
-        csv.writeLine(line)
+    if avail: csv.writeLine(line)
